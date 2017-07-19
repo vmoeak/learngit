a74f55e18405b58772183708e69d1dde666d319e
vmoeak <dangpengfei123@gmail.com>
2017-07-19 19:30:04 +0800

    add 1.c

---
+++ 1.c
@@ -0,0 +1,13 @@
+ ///
+ /// @file    1.c
+ /// @author  vmoeak(dangpengfei123@gmail.com)
+ /// @date    2017-07-19 18:06:05
+ ///
+ 
+#include <iostream>
+using std::cout;
+using std::endl;
+int main()
+{
+	cout<<"hello world\n";
+}
